01bd32a3ab7e45f26baa58a89b355d21aacf0083
Richard Machado <mmachado.richard@gmail.com>
2021-05-26 10:52:27 -0700

    adds single Stylesheet for rovers

---
+++ Screens/Rovers/styles/RoverStyles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  date: {
+    justifyContent: "center",
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  pickers: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+  input: {
+    height: 50,
+    width: 65,
+    marginLeft: 5,
+    borderWidth: 1,
+  },
+  photos: {
+    height: 375,
+    width: 375,
+    marginLeft: 25,
+  },
+});
