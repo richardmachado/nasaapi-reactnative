ad7353b2d518e23d2f4e78f89e2eb13b48808446
Richard Machado <mmachado.richard@gmail.com>
2021-06-03 11:07:54 -0700

    moved styles to a stylesheet

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { View, Button, Image } from "react-native";
+import { View, Button, Image, StyleSheet } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.body}>
       <Image
-        style={{ width: 375, height: 325 }}
+        style={styles.nasaphoto}
         source={{
           uri: "https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.jpg",
         }}
       />
-      <View style={{ marginBottom: 30, marginTop: 20 }}>
+      <View style={styles.buttons}>
         <Button
           title="Rovers Home Screen"
           onPress={() => navigation.navigate("RoversHomeScreen")}
@@ -31,3 +31,19 @@ export default function HomeScreen({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  nasaphoto: {
+    width: 375,
+    height: 325,
+  },
+  body: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttons: {
+    marginBottom: 30,
+    marginTop: 20,
+  },
+});
